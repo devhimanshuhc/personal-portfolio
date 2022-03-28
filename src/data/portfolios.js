cea42925bd024818f0e1e2b6cdf65f34bda6d6be
Himanshu <devhimanshuhc@gmail.com>
2022-03-28 18:15:05 +0530

    portfolio update

--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -1,10 +1,19 @@
-import img3 from "../img/portImages/news.png";
+import img3 from "../img/portImages/amazon.png";
 import img2 from "../img/portImages/resp.png";
 import img1 from "../img/portImages/trvlr.png";
 
 const portfolios = [
   {
     id: 1,
+    category: "Javascript",
+    image: img3,
+    link1: "https://github.com/devhimanshuhc/amazon-clone",
+    link2: "https://cloneamazon.vercel.app/",
+    title: "Amazon Clone",
+    text: "It is an Amazon Clone.",
+  },
+  {
+    id: 2,
     category: "Javascript",
     image: img1,
     link1: "https://github.com/devhimanshuhc/travel-site",
@@ -13,22 +22,13 @@ const portfolios = [
     text: "It's a traveling website where you can book your vacation destination",
   },
   {
-    id: 2,
+    id: 3,
     category: "Javascript",
     image: img2,
     link1: "https://github.com/devhimanshuhc/responsive-website",
     link2: "https://responsiveweb.vercel.app/",
     title: "Responsive website",
     text: "This is a responsive site creted HTML, CSS and Javascript",
-  },
-  {
-    id: 3,
-    category: "Javascript",
-    image: img3,
-    link1: "https://github.com/devhimanshuhc/newsApp",
-    link2: "https://newsnet.vercel.app/",
-    title: "NewsNet",
-    text: "It is a responsive news web app where you can get daily news doses.",
   },
 ];
 
